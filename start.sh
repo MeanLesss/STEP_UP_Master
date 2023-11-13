38e12a0d10cb09eea2bcbc56d36b0b6753614747
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2023-11-14 00:03:34 +0700

    fg

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # npm install
 composer install
 
-chown -R www-data:www-data /var/www/html
+chown -R $USER:www-data /var/www/html
 
 # Check if the directories exist before changing their ownership and permissions
 if [ -d "storage" ]; then \
