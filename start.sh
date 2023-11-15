2affd8c2dd54e1be6eebddbc800b1e8839154c61
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2023-11-15 20:26:57 +0700

    sdf

--- start.sh
+++ start.sh
@@ -5,10 +5,12 @@ composer install
 # chown -R www-data:$USER /var/www/html
 
 # Check if the directories exist before changing their ownership and permissions
+
 if [ -d "storage" ]; then \
     chown -R $USER:www-data storage && \
     chmod -R 775 storage; \
 fi
+
 if [ -d "bootstrap/cache" ]; then \
     chown -R $USER:www-data bootstrap/cache && \
     chmod -R 775 bootstrap/cache; \
@@ -17,8 +19,11 @@ fi
 if [ -d "public" ]; then \
     php artisan storage:link
 fi
+
 php artisan config:clear
 php artisan cache:clear
+
+
 # Exit the script
 exit 0
 # php-fpm
