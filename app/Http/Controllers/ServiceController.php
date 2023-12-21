6eaefa3aa6890cb6e031404e39cc52c402c44d97
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2023-12-21 21:43:00 +0700

    updated view image

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -133,7 +133,7 @@ class ServiceController extends Controller
                             $encryptedName = base64_encode($nameWithoutExtension);
                             $encryptedNameWithExtension = $encryptedName . '.' . $extension;
                             $path = 'uploads/'. Auth::user()->id;
-                            $file->storeAs('public/'.$path, $encryptedNameWithExtension);
+                            $file->storeAs('storage/'.$path, $encryptedNameWithExtension);
 
                             $filePaths[$originalName] = $path . '/' . $encryptedNameWithExtension;
                         }
@@ -255,7 +255,7 @@ class ServiceController extends Controller
                             $encryptedName = base64_encode($nameWithoutExtension);
                             $encryptedNameWithExtension = $encryptedName . '.' . $extension;
                             $path = 'uploads/'. Auth::user()->id;
-                            $file->storeAs('public/'.$path, $encryptedNameWithExtension);
+                            $file->storeAs('storage/'.$path, $encryptedNameWithExtension);
 
                             $filePaths[$originalName] = $path . '/' . $encryptedNameWithExtension;
                         }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,49 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Master page</title>
 </head>
+
 <body>
     <h1>Master page</h1>
-    <h1>{{Auth::user()->name}}</h1>
-    <img src="{{ asset('storage/uploads/1/Screenshot 2023-11-23 200712.png') }}" class="mx-auto d-block"
-    style="max-width: 100%;" />
+    <h1>{{ Auth::user()->name }}</h1>
+    <img src="{{ asset('storage/uploads/1/aW1hZ2VfMjAyMy0xMi0xN18wMS00Ny0zOA==.png') }}"
+     class="mx-auto d-block"
+        style="max-width: 100%;" />
+    <div id="aa">
+
+    </div>
+
+
+    <script src="{{ asset('plugins/js/jquery.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('plugins/js/sweetalert2.all.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            var settings = {
+                "url": "http://localhost:8000/api/service/32/view",
+                "method": "GET",
+                "timeout": 0,
+                "headers": {
+                    "X-CSRF-TOKEN": "",
+                    "Content-Type": "application/json"
+                },
+                "data": " ",
+                "success": function(response) {
+                    if(response.data.attachments != null){
+                        $.each(response.data.attachments, function(key, value) {
+                            $('#aa').append('<img src="' + value + '" alt="' + key + '" />');
+                        });
+                    }
+                }
+            };
+
+            $.ajax(settings).done(function(response) {
+                console.log(response);
+            });
+        });
+    </script>
+
 </body>
+
 </html>
