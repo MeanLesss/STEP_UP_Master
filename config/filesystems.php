337f78cf4c180210457dd86a85fa3d238626cf7d
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2024-02-04 22:59:24 +0700

    asdf

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,18 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
+
 
         's3' => [
             'driver' => 's3',
