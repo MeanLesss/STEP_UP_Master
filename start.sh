1efefefbce16850588c584ff7b39bc63e184b599
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2024-02-04 22:48:57 +0700

    saf

--- start.sh
+++ start.sh
@@ -9,12 +9,14 @@ composer install
 
 # Check if the directories exist before changing their ownership and permissions
 if [ -d "/var/www/html/storage" ]; then
-    chown -R $USER:www-data /var/www/html/storage && \
+    # chown -R $USER:www-data /var/www/html/storage && \
+    chown -R www-data:www-data /var/www/html/storage && \
     chmod -R 775 /var/www/html/storage
 fi
 
 if [ -d "/var/www/html/bootstrap/cache" ]; then
-    chown -R $USER:www-data /var/www/html/bootstrap/cache && \
+    # chown -R $USER:www-data /var/www/html/bootstrap/cache && \
+    chown -R www-data:www-data /var/www/html/bootstrap/cache && \
     chmod -R 775 /var/www/html/bootstrap/cache
 fi
 
@@ -22,7 +24,8 @@ if [ -d "/var/www/html/public" ]; then
     /usr/local/bin/php artisan storage:link
 fi
 
-chown -R $USER:www-data /var/www/html/public
+# chown -R $USER:www-data /var/www/html/public
+chown -R www-data:www-data /var/www/html/public
 chmod -R 775 /var/www/html/public
 
 /usr/local/bin/php artisan config:clear
